3e77d10debea7a19c0187a69e1d5ee2aade96712
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-12-21 08:50:11 -0300

    feat: integrate React Query by wrapping App component with QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { RouterProvider } from "react-router"
 import { router } from "./router/router"
 import { ThemeToggle } from "./components/themeToggle"
-import { Toaster} from '@/components/ui/toaster'
+import { Toaster } from '@/components/ui/toaster'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 function App() {
+
+  const queryClient = new QueryClient()
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeToggle />
       <div
-         className='min-h-screen bg-[var(--background)] text-[var(--foreground)] dark:bg-[var(--background)] dark:text-[var(--foreground)]'
+        className='min-h-screen bg-[var(--background)] text-[var(--foreground)] dark:bg-[var(--background)] dark:text-[var(--foreground)]'
       >
         <RouterProvider router={router} />
       </div>
       <Toaster />
-    </>
+    </QueryClientProvider>
+
   )
 }
 
