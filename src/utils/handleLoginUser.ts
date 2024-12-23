defce53508983d96b9afb7b9b3877b44fbc2b885
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-12-23 17:09:37 -0300

    feat: include user token and displayName in user store upon login

--- src/utils/handleLoginUser.ts
+++ src/utils/handleLoginUser.ts
@@ -4,13 +4,15 @@ import { useUserStore } from '@/store/useUserStore'
 export async function handleLoginUser(userCredential: UserCredential) {
   const { uid, displayName, email, photoURL } = userCredential.user
   const store = useUserStore.getState()
+  const token = await userCredential.user.getIdToken()
 
   store.setUser({
     uid,
     username: displayName || null,
     email: email || '',
     photoURL: photoURL || null,
-    name: displayName || null
+    token: token,
+    displayName: displayName || null,
   })
 
   store.isAuthenticated = true
